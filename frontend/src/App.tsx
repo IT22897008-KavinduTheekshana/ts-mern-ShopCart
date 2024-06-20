c8a96c3d0a1e4af8f528b16f284cf192ea6c2a47
KAVINDU <it22897008@my.sliit.lk>
2024-06-20 16:17:45 +0530

    Create-React-Context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
-import {  Container, Nav, Navbar} from 'react-bootstrap'
+import {  Button, Container, Nav, Navbar} from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
+import { Store } from './Store'
+import { useContext, useEffect } from 'react'
 
 
 function App() {
-  
+  const {
+    state: { mode },dispatch,} = useContext(Store)
+
+  useEffect(() => {
+    document.body.setAttribute('data-bs-theme', mode)
+  }, [mode])
+
+  const switchModeHandler = () => {
+    dispatch({ type: 'SWITCH_MODE' })
+  }
 
   return (
     <div className='d-flex flex-column h-full'>
       <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar expand="lg">
             <Container>
               <Navbar.Brand>tsamazona</Navbar.Brand>
             </Container>
             <Nav>
+              
+            <Button variant={mode} onClick={switchModeHandler}>
+              <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
+            </Button>
+
               <a href="/cart" className="nav-link">
                 Cart
               </a>
