90d4539a8185bde35b2bb5e02844599a7a02cc9e
KAVINDU <it22897008@my.sliit.lk>
2024-06-24 21:49:16 +0530

    update sign in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,41 +1,41 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express from 'express'
-import mongoose from 'mongoose'
-import { productRouter } from './routers/productRouter'
-import { seedRouter } from './routers/seedRouter'
-import { userRouter } from './routers/userRouter'
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import { productRouter } from './routers/productRouter';
+import { seedRouter } from './routers/seedRouter';
+import { userRouter } from './routers/userRouter';
 
-dotenv.config()
+dotenv.config();
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/tsmernamazonadb'
-mongoose.set('strictQuery', true)
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/tsmernamazonadb';
+
+mongoose.set('strictQuery', true);
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
-    console.log('connected to mongodb')
+    console.log('connected to mongodb');
   })
   .catch(() => {
-    console.log('error mongodb')
-  })
+    console.log('error mongodb');
+  });
 
-const app = express()
+const app = express();
 app.use(
   cors({
     credentials: true,
     origin: ['http://localhost:5173'],
   })
-)
+);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/products', productRouter)
-app.use('/api/users', userRouter)
-app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/seed', seedRouter);
 
-const PORT = 4000
+const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`server started at http://localhost:${PORT}`)
-})
+  console.log(`server started at http://localhost:${PORT}`);
+});
